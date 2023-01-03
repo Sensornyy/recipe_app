4c8ab7fbde9332540c0e13eadbcc0bcb62043e05
Sensornyy <maksym.koriak@chisw.com>
2023-01-03 15:59:42 +0200

    feat: add recipe model add json_serializable package

---
+++ lib/feature/data/models/recipe_model.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'package:recipe_app/feature/domain/entities/recipe_entity.dart';
+
+part 'recipe_model.g.dart';
+
+@JsonSerializable()
+class RecipeModel extends RecipeEntity {
+  const RecipeModel({
+    required super.title,
+    required super.calories,
+    required super.thumbnail,
+    required super.image,
+    required super.ingredients,
+  });
+
+  factory RecipeModel.fromJson(Map<String, dynamic> json) => _$RecipeModelFromJson(json);
+}
